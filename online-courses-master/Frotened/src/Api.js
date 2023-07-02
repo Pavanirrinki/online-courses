f8196a567349cc2340d4b9fc9e4101ff083c8f61
unknown <pavankumarirrinki0123@gmail.com>
2023-07-03 02:18:14 +0530

    Track large files using Git LFS

--- online-courses-master/Frotened/src/Api.js
+++ online-courses-master/Frotened/src/Api.js
@@ -1,2 +1,2 @@
 // export const API = 'https://online-courses-ebon.vercel.app/'
-export const API = 'https://online-courses-hiim.vercel.app/'
+export const API = 'https://online-courses-ten.vercel.app/'
