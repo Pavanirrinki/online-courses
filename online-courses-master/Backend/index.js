df098316075d4527144520e4262de19d81417b92
unknown <pavankumarirrinki0123@gmail.com>
2023-07-13 01:24:09 +0530

    header-changes

--- online-courses-master/Backend/index.js
+++ online-courses-master/Backend/index.js
@@ -10,8 +10,6 @@ require("dotenv").config()
 
 const app= express()
 
-
-
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:true}).then
 (()=>console.log("DB CONNECTED8"))
--- online-courses-master/Frotened/src/Api.js
+++ online-courses-master/Frotened/src/Api.js
@@ -1,3 +1,3 @@
 
- export const API = 'https://online-courses-ten.vercel.app/'
+ export const API = 'https://online-courses-lake.vercel.app/'
 // export const API = 'http://localhost:4678/'
