fdbe1df24660ff499b6ca2eeb40c4109adf27c6e
unknown <pavankumarirrinki0123@gmail.com>
2023-07-12 22:16:34 +0530

    new-changes

--- online-courses-master/Frotened/src/Api.js
+++ online-courses-master/Frotened/src/Api.js
@@ -1,3 +1,3 @@
 
-// export const API = 'https://online-courses-ten.vercel.app/'
-export const API = 'http://localhost:4678/'
+ export const API = 'https://online-courses-ten.vercel.app/'
+// export const API = 'http://localhost:4678/'
