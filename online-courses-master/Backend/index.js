f11cf7ebecfb2e67af7d666ce78f2a4b28efb769
unknown <pavankumarirrinki0123@gmail.com>
2023-07-12 23:12:56 +0530

    headers-changes

--- online-courses-master/Backend/index.js
+++ online-courses-master/Backend/index.js
@@ -11,16 +11,13 @@ require("dotenv").config()
 const app= express()
 
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://online-courses-mx2n.vercel.app');
-    next();
-  });
+
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:true}).then
 (()=>console.log("DB CONNECTED8"))
 
 app.use(cors({
-    origin:"*"
+    origin:"https://online-courses-mx2n.vercel.app"
 }))
 
   
