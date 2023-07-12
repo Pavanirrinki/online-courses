90fbb22351a84e58082b46357189ad3dcc09ede3
unknown <pavankumarirrinki0123@gmail.com>
2023-07-12 22:36:00 +0530

    new-changes

--- online-courses-master/Backend/index.js
+++ online-courses-master/Backend/index.js
@@ -17,6 +17,11 @@ mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:
 app.use(cors({
     origin:"*"
 }))
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://online-courses-mx2n.vercel.app');
+    next();
+  });
+  
 app.use(express.json());
 app.use('/', authRoutes);
 app.use("/",courseRoutes);
