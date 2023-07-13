154e6a6b5a97eb5515049fb94b58031e5bd6584b
unknown <pavankumarirrinki0123@gmail.com>
2023-07-13 18:17:27 +0530

    header-changes

--- online-courses-master/Frotened/src/Api.js
+++ online-courses-master/Frotened/src/Api.js
@@ -1,3 +1,3 @@
 
- export const API = 'https://online-courses-a7xc-bpnvxnfvx-pavanirrinki.vercel.app/'
+ export const API = 'https://online-courses-lzp5.vercel.app/'
 // export const API = 'http://localhost:4678/'
